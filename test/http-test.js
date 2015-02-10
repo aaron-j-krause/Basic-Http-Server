03574bbec37fa51fa228fec6114487872f1b8992
Aaron Krause <aaronjkrause@gmail.com>
2015-02-09 21:42:36 -0800

    test working, troubles w describe blocks

--- test/http-test.js
+++ test/http-test.js
@@ -1,25 +1,47 @@
-var app = require('../basic-http');
+require('../basic-http');
 var chai = require('chai');
 var chaihttp = require('chai-http');
 
-chai.use('chai-http');
+chai.use(chaihttp);
 
-var expect('chai').expect;
+var expect = chai.expect;
 
-describe('Basic Http', function(){
-  var server = 'localhost:3000'
-  describe('/time', function(){
-    it('should return a current date', function(done){
-      var day = (new Date()).getDay()
+describe('Basic Http', function() {
+  var server = 'localhost:3000';
+
+  describe('/time', function() {
+    it('should return a current date', function(done) {
+      var day = new Date().getDate().toString();
       chai.request(server)
         .get('/time')
-        .end(function(err,res){
+        .end(function(err, res) {
           expect(err).to.eql(null);
           expect(res).to.have.status(200);
-          expect(res.text)
-        })
+          expect(res.text).to.contain(day);
+        });
+      done();
+    });
 
-    })
+    it('should say hello', function(done) {
+      chai.request(server)
+        .get('/greet')
+        .end(function(err, res) {
+          expect(err).to.eql(null);
+          expect(res).to.have.status(200);
+          expect(res.text).to.contain('HELLO');
+        });
+      done();
+    });
 
-  })
-})
+    it('should take the name from the url', function(done) {
+      chai.request(server)
+        .get('/greet/dave')
+        .end(function(err, res) {
+          expect(err).to.eql(null);
+          expect(res).to.have.status(200);
+          expect(res.text).to.contain('dave');
+        });
+      done();
+    });
+  });
+});
