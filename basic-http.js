5a6582351c174db01468a463b36ab389138b202f
Aaron Krause <aaronjkrause@gmail.com>
2015-02-10 12:05:19 -0800

    added default branch, default name

--- basic-http.js
+++ basic-http.js
@@ -1,12 +1,14 @@
 var http = require('http');
 http.createServer(function(req, res) {
   var urlParts = req.url.split('/');
-  var name = urlParts[2];
-  var action = urlParts[1];
+  var name = urlParts[2] || 'ANONYMOUS';
+  var action = '/' + urlParts[1];
 
   var requestHandler = {
-    'time': (new Date()).toString() + '\n' ,
-    'greet': 'HELLO ' + name + '\n'
+    '/time': (new Date()).toString() + '\n' ,
+    '/greet': 'HELLO ' + name + '\n',
+    '/':'hit "/time" for the current time or' +
+    '"/greet/[yournamehere]" for a CUSTOM GREETING \n'
   };
 
   res.writeHead(200, {
